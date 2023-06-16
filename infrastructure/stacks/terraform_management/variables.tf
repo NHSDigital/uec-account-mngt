8e517b9448cbc3a071da13af244777b297135b8f
Jonathan Pearce <j.pearce1@nhs.net>
2023-06-16 13:06:17 +0100

    DR-74 Create terraform bucket and dynamodb

--- infrastructure/stacks/terraform_management/variables.tf
+++ infrastructure/stacks/terraform_management/variables.tf
@@ -1,10 +1,6 @@
-variable "project" {
-  description = "Project code typically reflects sub project of project owner eg nhse-uec-dos"
-}
-variable "project_owner" {
-  description = "Project owner based on orgnaistion and department code eg nhse-uec"
-}
-variable "environment" {
-  description = "The environment - dev, test, staging etc"
-}
+# ==============================================================================
+# Mandatory variables
 
+variable "account_name" {
+  description = "The name of the AWS account"
+}
