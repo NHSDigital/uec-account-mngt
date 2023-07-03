bb1406f690475eb5032334d1867b4740fd5785ec
Tim Rickwood <tim.rickwood@nhs.net>
2023-07-03 13:00:03 +0100

    DR-175 Change gitrepo name

--- infrastructure/shared/locals.tf
+++ infrastructure/shared/locals.tf
@@ -3,7 +3,11 @@
 
 locals {
 
+<<<<<<< main:infrastructure/shared/locals.tf
   terraform-git-repo = "uec-account-mngt"
+=======
+  terraform-git-repo = "uec-account-mgmt"
+>>>>>>> DR-175 Change gitrepo name:infrastructure/stacks/terraform_management/locals.tf
   account_alias      = data.aws_iam_account_alias.alias.account_alias
   account_id         = data.aws_caller_identity.current.id
 }
