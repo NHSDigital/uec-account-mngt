d6186ff76794559946522bf2582451591f8b3d33
Tim Rickwood <tim.rickwood@nhs.net>
2023-07-04 13:24:23 +0100

    DR-62 Create shared files

--- infrastructure/shared/locals.tf
+++ infrastructure/shared/locals.tf
@@ -3,11 +3,7 @@
 
 locals {
 
-<<<<<<< main:infrastructure/shared/locals.tf
   terraform-git-repo = "uec-account-mngt"
-=======
-  terraform-git-repo = "uec-account-mgmt"
->>>>>>> DR-175 Change gitrepo name:infrastructure/stacks/terraform_management/locals.tf
   account_alias      = data.aws_iam_account_alias.alias.account_alias
   account_id         = data.aws_caller_identity.current.id
 }
