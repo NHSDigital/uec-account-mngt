6e264a2a5c8d2214b5a81e59760a34cf3874e336
Tim Rickwood <tim.rickwood@nhs.net>
2023-07-05 12:50:45 +0100

    DR-175 Refactor for deploy

--- infrastructure/stacks/iam-policy/locals.tf
+++
@@ -1,9 +0,0 @@
-# ==============================================================================
-# Context
-
-locals {
-
-  terraform-git-repo = "uec-dos-account-mngt"
-  account_alias      = data.aws_iam_account_alias.alias.account_alias
-
-}
