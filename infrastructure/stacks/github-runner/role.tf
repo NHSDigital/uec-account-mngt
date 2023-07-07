f26e9a053e99093e00dabc685d794bc08b7ad2a6
Tim Rickwood <tim.rickwood@nhs.net>
2023-07-07 09:40:08 +0100

    DR-175 Add poweruser policy

--- infrastructure/stacks/github-runner/role.tf
+++ infrastructure/stacks/github-runner/role.tf
@@ -1,26 +1,36 @@
 
 # aws_iam_role_policy = an inline policy
 # aws_iam_policy, that is a managed policy and can be re-used
-
-resource "aws_iam_role_policy" "runner_policy_1" {
-  name = "Github_runner_policy"
-  role = aws_iam_role.github_runner_role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+# Get the policy by name
+data "aws_iam_policy" "power_user_policy" {
+  name = "PowerUserAccess"
 }
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "attach_power_user" {
+  role       = aws_iam_role.github_runner_role.name
+  policy_arn = data.aws_iam_policy.power_user_policy.arn
+}
+
+# resource "aws_iam_role_policy" "runner_policy_1" {
+#   name = "Github_runner_policy"
+#   role = aws_iam_role.github_runner_role.id
+
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression result to valid JSON syntax.
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = [
+#           "ec2:Describe*",
+#         ]
+#         Effect   = "Allow"
+#         Resource = "*"
+#       },
+#     ]
+#   })
+# }
 
 resource "aws_iam_role" "github_runner_role" {
   name               = "GitHub_Runner"
