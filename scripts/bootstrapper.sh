790be92b271bfe531b690735d5c4e8563f0346b7
Tim Rickwood <tim.rickwood@nhs.net>
2023-07-12 09:06:31 +0100

    DR-175 Working plan apply plan

--- scripts/bootstrapper.sh
+++ scripts/bootstrapper.sh
@@ -98,27 +98,28 @@ if ! $USE_REMOTE_STATE_STORE  ; then
     # run terraform init with migrate flag set
     terraform-init-migrate "$STACK" "$ENVIRONMENT" "$USE_REMOTE_STATE_STORE"
     # now push local state to remote
-    terraform state push "$STACK_DIR"/.terraform/terraform.tfstate
+    terraform state push "$STACK_DIR"/terraform.tfstate
     rm -f "$STACK_DIR"/locals.tf
     rm -f "$STACK_DIR"/provider.tf
     rm -f "$STACK_DIR"/versions.tf
+    # remove local terraform state to prevent clash when re-running eg to plan
+    rm -f "$STACK_DIR"/terraform.tfstate
     cd "$ROOT_DIR" || exit
   else
     export USE_REMOTE_STATE_STORE=false
   fi
 fi
 
-# TODO restore
-# # ------------- Step three create  thumbprint for github actions -----------
-# export HOST=$(curl https://token.actions.githubusercontent.com/.well-known/openid-configuration)
-# export CERT_URL=$(jq -r '.jwks_uri | split("/")[2]' <<< $HOST)
-# export THUMBPRINT=$(echo | openssl s_client -servername "$CERT_URL" -showcerts -connect "$CERT_URL":443 2> /dev/null | tac | sed -n '/-----END CERTIFICATE-----/,/-----BEGIN CERTIFICATE-----/p; /-----BEGIN CERTIFICATE-----/q' | tac | openssl x509 -sha1 -fingerprint -noout | sed 's/://g' | awk -F= '{print tolower($2)}')
-# # ------------- Step four create oidc identity provider, github runner role and policies for that role -----------
-# export TF_VAR_repo_name=$REPO_NAME
-# export TF_VAR_oidc_provider_url="https://token.actions.githubusercontent.com"
-# export TF_VAR_oidc_thumbprint=$THUMBPRINT
-# export TF_VAR_oidc_client="sts.amazonaws.com"
-# export STACK=github-runner
-# /bin/bash ./scripts/infra-deploy.sh
+# ------------- Step three create  thumbprint for github actions -----------
+export HOST=$(curl https://token.actions.githubusercontent.com/.well-known/openid-configuration)
+export CERT_URL=$(jq -r '.jwks_uri | split("/")[2]' <<< $HOST)
+export THUMBPRINT=$(echo | openssl s_client -servername "$CERT_URL" -showcerts -connect "$CERT_URL":443 2> /dev/null | tac | sed -n '/-----END CERTIFICATE-----/,/-----BEGIN CERTIFICATE-----/p; /-----BEGIN CERTIFICATE-----/q' | tac | openssl x509 -sha1 -fingerprint -noout | sed 's/://g' | awk -F= '{print tolower($2)}')
+# ------------- Step four create oidc identity provider, github runner role and policies for that role -----------
+export TF_VAR_repo_name=$REPO_NAME
+export TF_VAR_oidc_provider_url="https://token.actions.githubusercontent.com"
+export TF_VAR_oidc_thumbprint=$THUMBPRINT
+export TF_VAR_oidc_client="sts.amazonaws.com"
+export STACK=github-runner
+/bin/bash ./scripts/infra-deploy.sh
 
 
