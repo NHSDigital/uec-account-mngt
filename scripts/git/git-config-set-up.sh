c889116ed53f1656a3d36ff94dd5ce3ac621117a
pira Maheswaran <piravinth.maheswaran1@nhs.net>
2023-07-25 12:46:57 +0100

    DR-320 Git standard config

--- scripts/git/git-config-set-up.sh
+++ scripts/git/git-config-set-up.sh
@@ -7,3 +7,16 @@ chmod +x .git/hooks/pre-commit
 rm -f .git/hooks/commit-msg
 cp scripts/git/commit-msg .git/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
+
+git config branch.autosetupmerge false    
+git config branch.autosetuprebase always    
+git config commit.gpgsign true    
+git config core.autocrlf input    
+git config core.filemode true    
+git config core.hidedotfiles false    
+git config core.ignorecase false    
+git config pull.rebase true    
+git config push.default current    
+git config push.followTags true    
+git config rebase.autoStash true    
+git config remote.origin.prune true git 
