ca335c831cfab349d32ba078c92c8fce979ea7e8
Tim Rickwood <tim.rickwood@nhs.net>
2023-11-01 13:35:40 +0000

    DR-505 Protect tf locks and state buckets

--- infrastructure/modules/dynamodb/main.tf
+++ infrastructure/modules/dynamodb/main.tf
@@ -2,11 +2,11 @@ module "dynamodb_table" {
   source  = "terraform-aws-modules/dynamodb-table/aws"
   version = "3.3.0"
 
-  name                = var.table_name
-  hash_key            = var.hash_key
-  autoscaling_enabled = var.autoscaling_enabled
-
-  attributes = var.attributes
+  name                        = var.table_name
+  hash_key                    = var.hash_key
+  autoscaling_enabled         = var.autoscaling_enabled
+  deletion_protection_enabled = var.deletion_protection_enabled
+  attributes                  = var.attributes
 
   server_side_encryption_enabled = true
 }
--- infrastructure/stacks/terraform_management/dynamodb.tf
+++ infrastructure/stacks/terraform_management/dynamodb.tf
@@ -1,7 +1,7 @@
 module "terraform_lock_dynamodb" {
   source     = "../../modules/dynamodb"
   table_name = var.terraform_lock_table_name
-
+  deletion_protection_enabled = true
   hash_key = "LockID"
   attributes = [{
     name = "LockID"
