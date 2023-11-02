a7b0d23b6012d5e2489e0f91418db02f66582e47
Tim Rickwood <tim.rickwood@nhs.net>
2023-11-02 13:18:22 +0000

    DR-505 Protect tf state locks by iam

--- infrastructure/stacks/terraform_management/dynamodb.tf
+++ infrastructure/stacks/terraform_management/dynamodb.tf
@@ -1,8 +1,8 @@
 module "terraform_lock_dynamodb" {
-  source     = "../../modules/dynamodb"
-  table_name = var.terraform_lock_table_name
+  source                      = "../../modules/dynamodb"
+  table_name                  = var.terraform_lock_table_name
   deletion_protection_enabled = true
-  hash_key = "LockID"
+  hash_key                    = "LockID"
   attributes = [{
     name = "LockID"
     type = "S"
