742e7f2d46d14f7024084c57aa7245f2d7b993f9
Tim Rickwood <tim.rickwood@nhs.net>
2023-11-08 10:11:44 +0000

    DR-505 Refactor workflows to align more closely with domain repos (#29)
    <!-- markdownlint-disable-next-line first-line-heading -->
    ## Description
    As part of the work on original DR-505 decision made to refactor the
    code to align it more closely with the workflow names and structures etc
    being rolled out to the various domain repos. It is acknowledged that we
    may refactor all again but this exercise should make that exercise
    easier
    
    
    <!-- Describe your changes in detail. -->
    
    ## Context
    
    <!-- Why is this change required? What problem does it solve? -->
    
    ## Type of changes
    
    <!-- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply. -->
    
    - [ ] Refactoring (non-breaking change)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would change existing
    functionality)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    - [ ] I am familiar with the [contributing
    guidelines](../docs/CONTRIBUTING.md)
    - [ ] I have followed the code style of the project
    - [ ] I have added tests to cover my changes
    - [ ] I have updated the documentation accordingly
    - [ ] This PR is a result of pair or mob programming
    
    ---
    
    ## Sensitive Information Declaration
    
    To ensure the utmost confidentiality and protect your and others
    privacy, we kindly ask you to NOT including [PII (Personal Identifiable
    Information) / PID (Personal Identifiable
    Data)](https://digital.nhs.uk/data-and-information/keeping-data-safe-and-benefitting-the-public)
    or any other sensitive data in this PR (Pull Request) and the codebase
    changes. We will remove any PR that do contain any sensitive
    information. We really appreciate your cooperation in this matter.
    
    - [ ] I confirm that neither PII/PID nor sensitive data are included in
    this PR and the codebase changes.

--- scripts/bootstrapper.sh
+++ scripts/bootstrapper.sh
@@ -75,7 +75,7 @@ else
 fi
 
 # deploy stack
-/bin/bash ./scripts/infra-deploy.sh
+/bin/bash ./scripts/action-infra-stack.sh
 
 # having build the stack using a local backend we need to push
 # the state to the remote
@@ -115,6 +115,6 @@ export TF_VAR_oidc_provider_url="https://token.actions.githubusercontent.com"
 export TF_VAR_oidc_thumbprint=$THUMBPRINT
 export TF_VAR_oidc_client="sts.amazonaws.com"
 export STACK=github-runner
-/bin/bash ./scripts/infra-deploy.sh
+/bin/bash ./scripts/action-infra-stack.sh
 
 
